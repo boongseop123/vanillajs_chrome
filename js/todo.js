cd72ff925b410d35bd9e6059f89e9b815e69d193
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-03-13 18:20:41 +0900

    [add] make todoList

--- js/todo.js
+++ js/todo.js
@@ -2,8 +2,23 @@ const todoForm=document.getElementById("todo-form");
 const todoList=document.getElementById("todo-list");
 const todoInput=document.querySelector("#todo-form input");
 
-function paintTodo(newTodo){
 
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintTodo(newTodo){
+    console.log("i will paint",newTodo);
+    const li=document.createElement("li");
+    const span=document.createElement("span");
+    span.innerText=newTodo;
+    const button=document.createElement("button");
+    button.innerText="❌";
+    button.addEventListener("click",deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
 }
 
 
