464807f3e0b3ed44abd50840e16af11f65136aae
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-03-14 20:15:06 +0900

    [add] make deleting function

--- js/todo.js
+++ js/todo.js
@@ -3,17 +3,21 @@ const todoList=document.getElementById("todo-list");
 const todoInput=document.querySelector("#todo-form input");
 
 const TODOS_KEY="todos";
-const toDos=[];
+
+let toDos=[];
 
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
+    toDos=toDos.filter(toDo=>toDo.id!==parseInt(li.id));
+    saveToDos();
 }
 
 function paintTodo(newTodo){
     const li=document.createElement("li");
+    li.id=newTodo.id;
     const span=document.createElement("span");
-    span.innerText=newTodo;
+    span.innerText=newTodo.text;
     const button=document.createElement("button");
     button.innerText="❌";
     button.addEventListener("click",deleteToDo);
@@ -31,8 +35,12 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo=todoInput.value;
     todoInput.value="";
-    toDos.push(newTodo);
-    paintTodo(newTodo);
+    const newTodoObj={
+        text:newTodo,
+        id:Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveToDos();
 }
 
@@ -42,5 +50,6 @@ const savedToDos=localStorage.getItem(TODOS_KEY);
 
 if(savedToDos){
     const parsedToDos=JSON.parse(savedToDos);
+    toDos=parsedToDos;
    parsedToDos.forEach(paintTodo);
 }
